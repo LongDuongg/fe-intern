806e4f6a4089f2c9e55d5e3b849569c580063179
LongDuongg <long2622001@gmail.com>
2025-03-31 16:33:56 +0700

    test kebab to camel function

--- module1/test.js
+++ module1/test.js
@@ -1,3 +1,5 @@
-const { mergeStrings, uppercase } = require("./1-string");
+const { mergeStrings, kebabToCamel } = require("./1-string");
 
-console.log(mergeStrings("abc", "123456"));
+// console.log(mergeStrings("abc", "123456"));
+console.log(kebabToCamel("da-nang"));
+console.log(kebabToCamel("ho-chi-minh-city"));
