0e38bd5eff5bf7247a426d2a959c84612bbed95d
LongDuongg <long2622001@gmail.com>
2025-03-31 17:09:36 +0700

    test camel to kebab function

--- module1/test.js
+++ module1/test.js
@@ -1,5 +1,7 @@
-const { mergeStrings, kebabToCamel } = require("./1-string");
+const { mergeStrings, kebabToCamel, camelToKebab } = require("./1-string");
 
 // console.log(mergeStrings("abc", "123456"));
-console.log(kebabToCamel("da-nang"));
-console.log(kebabToCamel("ho-chi-minh-city"));
+// console.log(kebabToCamel("da-nang"));
+// console.log(kebabToCamel("ho-chi-minh-city"));
+console.log(camelToKebab("daNang"));
+console.log(camelToKebab("hoChiMinhCity"));
