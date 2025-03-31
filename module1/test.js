da9c73500edffd31efd59702d3c61d3a7b11360b
LongDuongg <long2622001@gmail.com>
2025-03-31 20:55:46 +0700

    test isUpperCase function

--- module1/test.js
+++ module1/test.js
@@ -1,7 +1,14 @@
-const { mergeStrings, kebabToCamel, camelToKebab } = require("./1-string");
+const {
+  mergeStrings,
+  kebabToCamel,
+  camelToKebab,
+  isUpperCase,
+} = require("./1-string");
 
 // console.log(mergeStrings("abc", "123456"));
 // console.log(kebabToCamel("da-nang"));
 // console.log(kebabToCamel("ho-chi-minh-city"));
-console.log(camelToKebab("daNang"));
-console.log(camelToKebab("hoChiMinhCity"));
+// console.log(camelToKebab("daNang"));
+// console.log(camelToKebab("hoChiMinhCity"));
+console.log(isUpperCase("A"));
+console.log(isUpperCase("a"));
