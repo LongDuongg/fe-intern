c0a9a40da7edd39cdd1543710c754b20ee8787f1
LongDuongg <long2622001@gmail.com>
2025-04-01 18:22:00 +0700

    add white space between console log for better view

--- module1/test.js
+++ module1/test.js
@@ -22,17 +22,27 @@ const people = [
 ];
 
 // console.log(mergeStrings("abc", "123456"));
+
 // console.log(kebabToCamel("da-nang"));
 // console.log(kebabToCamel("ho-chi-minh-city"));
+
 // console.log(camelToKebab("daNang"));
 // console.log(camelToKebab("hoChiMinhCity"));
+
 // console.log(isUpperCase("A"));
 // console.log(isUpperCase("a"));
+
 // console.log(upperLower(" a NiCE  dAY "));
+
 // console.log(capitalize(" a NiCE  dAY "));
+
 // console.log(trim(" a nice  day "));
+
 // console.log(trimAll(" a nice  day "));
+
 // console.log(formatNumber(100000000));
+
 // console.log(abba("ab", "cde"));
 // console.log(abba("Hi", " John "));
-console.log(table(people));
+
+// console.log(table(people));
