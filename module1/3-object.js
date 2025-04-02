9e066b97b25d1c085ce44dd6e0600c8cdcdc4a29
LongDuongg <long2622001@gmail.com>
2025-04-02 20:06:02 +0700

    No need to use get, set and clone anymore

--- module1/3-object.js
+++ module1/3-object.js
@@ -17,58 +17,7 @@ let school = {
   ],
 };
 
-const getData = (obj, keys) => {
-  // If there is no keys, return the object itself
-  if (keys === undefined || keys === null || keys.length === 0) {
-    return obj;
-  }
-
-  const [firstKey, ...restKeys] = keys;
-
-  if (Array.isArray(obj)) {
-    return obj
-      .map((item) => getData(item[firstKey], restKeys))
-      .reduce((acc, arr) => acc.concat(arr), []); // use reduce to flatten the array
-  }
-
-  return getData(obj[firstKey], restKeys);
-};
-
 // console.log(getData(school, ["classes", "pupils", "name"]));
-
-const setData = (obj, keys, value) => {
-  // If there is no keys, return the value itself
-  if (!keys || keys.length === 0) {
-    return value;
-  }
-
-  const [firstKey, ...restKeys] = keys;
-
-  // If the key doesn't exist, initialize it as an array if next key is a number, otherwise as an object
-  if (!(firstKey in obj)) {
-    obj[firstKey] = isNaN(restKeys[0]) ? {} : [];
-  }
-
-  obj[firstKey] = setData(obj[firstKey], restKeys, value);
-  return obj;
-};
-
-const clone = (input) => {
-  if (input === null || typeof input !== "object") {
-    return input; // Return the value if input is not an object
-  }
-
-  if (Array.isArray(input)) {
-    return input.map((item) => clone(item)); // Recursively clone each item in the array
-  }
-
-  let ret = {}; // Create an object to hold the cloned values
-
-  for (const key in input) {
-    ret[key] = input[key]; // Recursively clone each property
-  }
-  return ret;
-};
 
 // 1.Add pupil named Jack to class 1A.
 // console.dir(setData(school, ["classes", 0, "pupils", 2], { name: "Jack" }));
