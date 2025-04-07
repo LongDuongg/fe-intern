24777b6c3c1ea8002f2b0a606c98eddd51ded12c
LongDuongg <long2622001@gmail.com>
2025-04-07 15:01:09 +0700

    logging utility

--- module2/2-partial.js
+++ module2/2-partial.js
@@ -12,10 +12,27 @@ const formatUSD = createCurrencyFormatter("en-US", "USD");
 const formatEUR = createCurrencyFormatter("de-DE", "EUR");
 const formatJPY = createCurrencyFormatter("ja-JP", "JPY");
 
-console.log(`"${formatUSD(1000)}"`);
-console.log(`"${formatEUR(1000)}"`);
-console.log(`"${formatJPY(1000)}"`);
+// console.log(`"${formatUSD(1000)}"`);
+// console.log(`"${formatEUR(1000)}"`);
+// console.log(`"${formatJPY(1000)}"`);
 
 // 2 Logging Utility
+const log = (level) => {
+  return function (message) {
+    let arr = [];
+    for (const letter of level) {
+      arr.push(letter.toUpperCase());
+    }
+    return `[${arr.join("")}] ${message}`;
+  };
+};
+
+const info = log("info");
+const warn = log("warn");
+const error = log("error");
+
+console.log(info("Application started"));
+console.log(warn("Low disk space"));
+console.log(error("Server is down!"));
 
 // 3 Rate Limiter
