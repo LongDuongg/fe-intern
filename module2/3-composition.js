c9629995efb366127d25703eb9e9cadbf81ff3e6
LongDuongg <long2622001@gmail.com>
2025-04-08 18:19:44 +0700

    Sanitize User Input

--- module2/3-composition.js
+++ module2/3-composition.js
@@ -42,18 +42,40 @@ const formatUser = (user) => addTitle(toUpperCase(getFullName(user)));
 
 const formatUser2 = (user) => pipe(getFullName, toUpperCase, addTitle)(user);
 
-console.log(formatUser({ firstName: "John", lastName: "Doe" }));
-console.log(formatUser({ firstName: "Jane" })); //
-console.log(formatUser({}));
+// console.log(formatUser({ firstName: "John", lastName: "Doe" }));
+// console.log(formatUser({ firstName: "Jane" })); //
+// console.log(formatUser({}));
 
-console.log(formatUser({ firstName: "John", lastName: "Doe" }));
-console.log(formatUser({ firstName: "Jane" })); //
-console.log(formatUser({}));
+// console.log(formatUser({ firstName: "John", lastName: "Doe" }));
+// console.log(formatUser({ firstName: "Jane" })); //
+// console.log(formatUser({}));
+
+// =========================================================================
 
 // Sanitize User Input
+const trim = (str) => str.trim();
+
+const toLowerCase = (str) => str.toLowerCase();
+
+const removeSpecialChars = (str) => str.replace(/[^a-zA-Z0-9 ]/g, "");
+
+const limitLength = (max) => (str) =>
+  str.length > max ? "exceed max limit" : `"${str}"`;
+
+// composition of the above functions
+const sanitizeInput = (str) =>
+  limitLength(10)(removeSpecialChars(toLowerCase(trim(str))));
+
+const sanitizeInput2 = (str) =>
+  pipe(trim, toLowerCase, removeSpecialChars, limitLength(100))(str);
+
+console.log(sanitizeInput("   Hello@WORLD!!! Welcome to JavaScript.  "));
+console.log(sanitizeInput2("   Hello@WORLD!!! Welcome to JavaScript.  "));
 
 // Display Prices
 
 // Transform API Response
 
 // Generate SEO-Friendly URLs
+
+//
