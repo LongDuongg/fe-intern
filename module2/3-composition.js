899d689f606cae554a4db5743c3ab1bac290c047
LongDuongg <long2622001@gmail.com>
2025-04-08 16:23:26 +0700

    Format and Display User Information

--- module2/3-composition.js
+++ module2/3-composition.js
@@ -20,10 +20,35 @@ const square = (x) => x * x;
 
 const triple = (x) => x * 3;
 
-console.log(pipe(plus1, double, square, triple)(2));
-console.log(pipe2(plus1, double, square, triple)(2));
+// console.log(pipe(plus1, double, square, triple)(2));
+// console.log(pipe2(plus1, double, square, triple)(2));
+
+// =========================================================================
 
 // Format and Display User Information
+const getFullName = (user) => {
+  const { firstName = "", lastName = "" } = user;
+  return `${firstName} ${lastName}`;
+};
+
+const toUpperCase = (str) => str.toUpperCase();
+
+const addTitle = (name) => {
+  return name ? `Mr./Ms. ${name}` : "Unknown User";
+};
+
+// composition of the above functions
+const formatUser = (user) => addTitle(toUpperCase(getFullName(user)));
+
+const formatUser2 = (user) => pipe(getFullName, toUpperCase, addTitle)(user);
+
+console.log(formatUser({ firstName: "John", lastName: "Doe" }));
+console.log(formatUser({ firstName: "Jane" })); //
+console.log(formatUser({}));
+
+console.log(formatUser({ firstName: "John", lastName: "Doe" }));
+console.log(formatUser({ firstName: "Jane" })); //
+console.log(formatUser({}));
 
 // Sanitize User Input
 
