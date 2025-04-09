652a07cd13a411de7222cf95bf07a72f362fc3e6
LongDuongg <long2622001@gmail.com>
2025-04-09 11:44:59 +0700

    format all testing function into separate parts

--- module2/test.js
+++ module2/test.js
@@ -9,19 +9,67 @@ const {
   setData,
   clone,
   getData,
+  deepEqual,
 } = require("./1-array-object.js");
 
+const ha = { name: "Ha" };
+const luan = { name: "Luan" };
+const a = { name: "Ha", jobs: ["developer", "teacher"] };
+const a2 = { name: "Ha", jobs: ["developer", "teacher"] };
+
+// console.log(deepEqual(ha, luan));
+// console.log(deepEqual(a, a2));
+// console.log(deepEqual(null, undefined));
+
+// ======================================================================================================
+
 const array = [
+  ha,
   { name: "Quan" },
   { name: "Son" },
   { name: "Cuong" },
   { name: "Hung" },
 ];
 
+// console.log(filter(array, (item) => item.name.indexOf("u") > -1));
+// console.log(filter(array, (item) => item.name.indexOf("a") > -1));
+// console.log(array.filter2((item) => item.name.indexOf("x") > -1));
+// console.log(array.filter2((item) => item.name.length > 4));
+
+// console.log("array after remove");
+// console.log(remove(ha, array));
+// console.log(array.remove2({ name: "Son" }));
+
+// console.log(without(array, (item) => item.name === "Quan"));
+// console.log(array.without2((item) => item.name === "Cuong"));
+
+// console.log("=========================");
+// console.log("original array :");
+// console.log(array);
+
+// ======================================================================================================
+
+// console.log(merge({ name: "Quan" }, { age: 20 }));
+// console.log(
+//   merge({ name: "Quan", age: 23 }, { age: null }, { birthday: "26/02/2000" })
+// );
+
+// ======================================================================================================
+
 const listMap = {
   "Hoa": ["Le Thi Hoa", "Mai Hoa"],
   "Tham": ["Hong Tham", "Xuan Tham"],
 };
+
+// console.log(addToListMap("Hoa", "Nguyen Thi Hoa", listMap));
+// console.log(addToListMap("Son", "Anh Son", listMap));
+
+// console.log("=========================");
+
+// console.log("original object :");
+// console.log(listMap);
+
+// ======================================================================================================
 
 const numbArr = [3, 7, 23, 1, 5];
 
@@ -30,6 +78,13 @@ const people = [
   { name: "Luan", age: 24 },
   { name: "Hoa", age: 28 },
 ];
+
+// console.log(sortByDistance(numbArr, 5));
+
+// console.log(sort(people, (person) => person.name));
+// console.log(sort(people, (person) => person.age));
+
+// ======================================================================================================
 
 const person = {
   name: "Tung",
@@ -45,60 +100,32 @@ const person = {
   ],
 };
 
-// console.log(filter(array, (item) => item.name.indexOf("u") > -1));
-// console.log(filter(array, (item) => item.name.indexOf("a") > -1));
-// console.log(array.filter2((item) => item.name.indexOf("x") > -1));
-// console.log(array.filter2((item) => item.name.length > 4));
+// const hieu = clone(person);
 
-// console.log("array after remove");
-// console.log(remove({ name: "Cuong" }, array));
-// console.log(array.remove2({ name: "Son" }));
+// hieu.name = "Hieu";
+// hieu.age = 25;
+// hieu.address.street = "Ba Trieu";
 
-// console.log(without(array, (item) => item.name === "Quan"));
-// console.log(array.without2((item) => item.name === "Cuong"));
+// console.log("Hieu :");
+// console.log(hieu);
 
-// console.log(merge({ name: "Quan" }, { age: 20 }));
-// console.log(
-//   merge({ name: "Quan", age: 23 }, { age: null }, { birthday: "26/02/2000" })
-// );
+// ======================================================================================================
 
-// console.log(addToListMap("Hoa", "Nguyen Thi Hoa", listMap));
+// console.log(getData(person, ["name"])););// console.log(getData(person, ["age"]));
+// console.log(getData(person, ["address", "city"]));
+// console.log(getData(person, ["address", "street"]));
+// console.log(getData(person, ["jobs", 0, "salary"]));
 
-// console.log(sortByDistance(numbArr, 5));
+// ======================================================================================================
 
-// console.log(sort(people, (person) => person.name));
-// console.log(sort(people, (person) => person.age));
+// console.log(setData(person, ["name"], "Nguyen Trai"));
+// console.log(setData(person, ["age"], 20));
+// console.log(setData(person, ["address", "city"], "Ha Giang"));
+// console.log(setData(person, ["address", "street"], "Ta Hien"));
+// console.log(setData(person, ["hobbies", 0], "gaming"));
+// console.log(setData(person, ["jobs", 0, "title"], "senior developer"));
+// console.log(setData(person, ["gender"], "male"));
 
-// =========================================================================
-
-const hieu = clone(person);
-
-hieu.name = "Hieu";
-hieu.age = 25;
-hieu.address.street = "Ba Trieu";
-
-console.log("Hieu :");
-console.log(hieu);
-
-console.log(getData(person, ["name"]));
-console.log(getData(person, ["age"]));
-console.log(getData(person, ["address", "city"]));
-console.log(getData(person, ["address", "street"]));
-
-// setData(person, ["name"], "Nguyen Trai");
-// setData(person, ["age"], 20);
-// setData(person, ["address", "city"], "Ha Giang");
-// setData(person, ["address", "street"], "Ta Hien");
-// setData(person, ["hobbies", 0], "gaming");
-// setData(person, ["jobs", 0, "title"], "senior developer");
-// setData(person, ["gender"], "male");
-
-console.log("=========================");
-// console.log("original array :");
-// console.log(array);
-// console.log(numbArr);
-// console.log(people);
-
-console.log("original object :");
-// console.log(listMap);
-console.log(person);
+// console.log("=========================");
+// console.log("original object :");
+// console.log(person);
