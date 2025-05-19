4f4ae15f5573b772063f91478a4de39bf3e094ae
LongDuongg <long2622001@gmail.com>
2025-05-19 15:22:22 +0700

    copy template and apply to the setted up routes

--- project2/real-world-app/src/App.jsx
+++ project2/real-world-app/src/App.jsx
@@ -1,18 +1,26 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 
+import { Home } from "./routes/home/home.jsx";
+import { Login } from "./routes/login/login.jsx";
+import { Signup } from "./routes/signup/signup.jsx";
+import { Setting } from "./routes/setting/setting.jsx";
+import { Article } from "./routes/article/article.jsx";
+import { ArticleForm } from "./routes/article/article-form.jsx";
+import { Profile } from "./routes/profile/profile.jsx";
+
 export const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<div>Home</div>} />
-      <Route path="/login" element={<div>Login</div>} />
-      <Route path="/register" element={<div>Register</div>} />
-      <Route path="/settings" element={<div>settings</div>} />
-      <Route path="/editor" element={<div>editor</div>} />
-      <Route path="/editor/:slug" element={<div>editor-slug</div>} />
-      <Route path="/article/:slug" element={<div>article</div>} />
-      <Route path="/profile/:username" element={<div>profile</div>} />
-      <Route path="/profile/:username/favorite" element={<div>favorite</div>} />
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Signup />} />
+      <Route path="/settings" element={<Setting />} />
+      <Route path="/editor" element={<ArticleForm />} />
+      <Route path="/editor/:slug" element={<ArticleForm />} />
+      <Route path="/article/:slug" element={<Article />} />
+      <Route path="/profile/:username" element={<Profile />} />
+      <Route path="/profile/:username/favorite" element={<Profile />} />
     </Routes>
   );
 };
