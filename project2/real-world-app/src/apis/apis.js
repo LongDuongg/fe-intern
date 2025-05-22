a79bf131327500032f3bdd1575ef0526b51a1168
LongDuongg <long2622001@gmail.com>
2025-05-22 10:17:51 +0700

    setup api login

---
+++ project2/real-world-app/src/apis/apis.js
@@ -0,0 +1,30 @@
+import { waitTimeout } from "../common/utils/async";
+
+export const user = {
+  login: async ({ email, password }) => {
+    await waitTimeout(1000);
+    return fetch(
+      "https://conduit-realworld-example-app.fly.dev/api/users/login",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          user: {
+            email,
+            password,
+          },
+        }),
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => data);
+  },
+
+  signUp: async ({ name, email, password }) => {},
+};
+
+const apis = { user };
+
+export default apis;
