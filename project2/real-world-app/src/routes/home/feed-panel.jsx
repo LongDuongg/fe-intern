711096a88e33c194fb2601a23b66aa91b68126cc
LongDuongg <long2622001@gmail.com>
2025-05-30 23:54:14 +0700

    display "Your feed" tab after login

--- project2/real-world-app/src/routes/home/feed-panel.jsx
+++ project2/real-world-app/src/routes/home/feed-panel.jsx
@@ -1,61 +1,62 @@
 import { cs } from "../../common/chain-services";
 
-import { TabHeader, Tabs } from "./tab";
+import { Tabs } from "./tab";
 import { ArticlePreviewList } from "./article-preview-list";
 import { consumeContext } from "../../common/react/context";
 
 export const FeedPanel = ({ selectedTag }) => {
-  return cs(consumeContext("apis"), ({ apis }) => {
-    const tabs = [
-      {
-        key: "your-feed",
-        label: "Your Feed",
-        // render: () => (
-        //   <ArticlePreviewList
-        //     key={"your-feed"}
-        //     getData={apis.article.getMyFeed}
-        //   />
-        // ),
-        render: () =>
-          ArticlePreviewList({
-            getData: apis.article.getMyFeed,
-          }),
-      },
-      {
-        key: "global-feed",
-        label: "Global Feed",
-        // render: () => (
-        //   <ArticlePreviewList
-        //     key={"global-feed"}
-        //     getData={apis.article.getGlobalFeed}
-        //   />
-        // ),
-        render: () =>
-          ArticlePreviewList({
-            getData: apis.article.getGlobalFeed,
-          }),
-      },
-    ];
+  return cs(
+    consumeContext("apis"),
+    consumeContext("auth"),
+    ({ apis, auth }) => {
+      const tabs = [
+        auth.user && {
+          key: "your-feed",
+          label: "Your Feed",
+          // render: () => (
+          //   <ArticlePreviewList
+          //     key={"your-feed"}
+          //     getData={apis.article.getMyFeed}
+          //   />
+          // ),
+          render: () =>
+            ArticlePreviewList({
+              getData: apis.article.getMyFeed,
+            }),
+        },
+        {
+          key: "global-feed",
+          label: "Global Feed",
+          // render: () => (
+          //   <ArticlePreviewList
+          //     key={"global-feed"}
+          //     getData={apis.article.getGlobalFeed}
+          //   />
+          // ),
+          render: () =>
+            ArticlePreviewList({
+              getData: apis.article.getGlobalFeed,
+            }),
+        },
+        selectedTag.value && {
+          key: `${selectedTag.value}`,
+          label: `#${selectedTag.value}`,
+          render: () =>
+            ArticlePreviewList({
+              getData: ({ page }) =>
+                apis.article.getFeedByTag({ page, tag: selectedTag.value }),
+            }),
+          forced: true,
+        },
+      ].filter((v) => v);
 
-    if (selectedTag.value) {
-      tabs.push({
-        key: `${selectedTag.value}`,
-        label: `#${selectedTag.value}`,
-        render: () =>
-          ArticlePreviewList({
-            getData: ({ page }) =>
-              apis.article.getFeedByTag({ page, tag: selectedTag.value }),
-          }),
-        forced: true,
+      return Tabs({
+        tabs,
+        initActive: auth.user ? 1 : 0,
+        onChangeTab: () => {
+          selectedTag.onChange(null);
+        },
       });
     }
-
-    return Tabs({
-      tabs,
-      initActive: 1,
-      onChangeTab: () => {
-        selectedTag.onChange(null);
-      },
-    });
-  });
+  );
 };
