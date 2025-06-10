46113cc2f47260dd34ce1e154704e4ccc3cc1fb1
LongDuongg <long2622001@gmail.com>
2025-06-10 10:31:15 +0700

    adjust file name to article-tabs

--- project2/real-world-app/src/routes/profile/profile.jsx
+++ project2/real-world-app/src/routes/profile/profile.jsx
@@ -6,7 +6,7 @@ import { EmptyFC } from "../../common/react/empty-fc.js";
 import { Layout } from "../layout/layout.jsx";
 import { Load2 } from "../../common/react/load2.js";
 import { FollowButton } from "../article/follow-button.jsx";
-import { ArticlePanel } from "./article-panel.jsx";
+import { ArticleTabs } from "./article-tabs.jsx";
 
 export const Profile = () =>
     cs(
@@ -77,100 +77,7 @@ export const Profile = () =>
                     <div className="container">
                         <div className="row">
                             <div className="col-xs-12 col-md-10 offset-md-1">
-                                {ArticlePanel({})}
-                                {/* <div className="articles-toggle">
-                                    <ul className="nav nav-pills outline-active">
-                                        <li className="nav-item">
-                                            <a className="nav-link active" href="">
-                                                My Articles
-                                            </a>
-                                        </li>
-                                        <li className="nav-item">
-                                            <a className="nav-link" href="">
-                                                Favorited Articles
-                                            </a>
-                                        </li>
-                                    </ul>
-                                </div>
-
-                                <div className="article-preview">
-                                    <div className="article-meta">
-                                        <a href="/profile/eric-simons">
-                                            <img src="http://i.imgur.com/Qr71crq.jpg" />
-                                        </a>
-                                        <div className="info">
-                                            <a href="/profile/eric-simons" className="author">
-                                                {auth.user.username}
-                                            </a>
-                                            <span className="date">January 20th</span>
-                                        </div>
-                                        <button className="btn btn-outline-primary btn-sm pull-xs-right">
-                                            <i className="ion-heart"></i> 29
-                                        </button>
-                                    </div>
-                                    <a
-                                        href="/article/how-to-buil-webapps-that-scale"
-                                        className="preview-link"
-                                    >
-                                        <h1>How to build webapps that scale</h1>
-                                        <p>This is the description for the post.</p>
-                                        <span>Read more...</span>
-                                        <ul className="tag-list">
-                                            <li className="tag-default tag-pill tag-outline">
-                                                realworld
-                                            </li>
-                                            <li className="tag-default tag-pill tag-outline">
-                                                implementations
-                                            </li>
-                                        </ul>
-                                    </a>
-                                </div>
-
-                                <div className="article-preview">
-                                    <div className="article-meta">
-                                        <a href="/profile/albert-pai">
-                                            <img src="http://i.imgur.com/N4VcUeJ.jpg" />
-                                        </a>
-                                        <div className="info">
-                                            <a href="/profile/albert-pai" className="author">
-                                                Albert Pai
-                                            </a>
-                                            <span className="date">January 20th</span>
-                                        </div>
-                                        <button className="btn btn-outline-primary btn-sm pull-xs-right">
-                                            <i className="ion-heart"></i> 32
-                                        </button>
-                                    </div>
-                                    <a href="/article/the-song-you" className="preview-link">
-                                        <h1>
-                                            The song you won't ever stop singing. No matter how hard
-                                            you try.
-                                        </h1>
-                                        <p>This is the description for the post.</p>
-                                        <span>Read more...</span>
-                                        <ul className="tag-list">
-                                            <li className="tag-default tag-pill tag-outline">
-                                                Music
-                                            </li>
-                                            <li className="tag-default tag-pill tag-outline">
-                                                Song
-                                            </li>
-                                        </ul>
-                                    </a>
-                                </div>
-
-                                <ul className="pagination">
-                                    <li className="page-item active">
-                                        <a className="page-link" href="">
-                                            1
-                                        </a>
-                                    </li>
-                                    <li className="page-item">
-                                        <a className="page-link" href="">
-                                            2
-                                        </a>
-                                    </li>
-                                </ul> */}
+                                {ArticleTabs({ profile: profile.value?.profile })}
                             </div>
                         </div>
                     </div>
