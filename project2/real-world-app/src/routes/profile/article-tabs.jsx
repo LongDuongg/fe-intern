229e547650f5b488c05f14ed0fd54e4b15458524
LongDuongg <long2622001@gmail.com>
2025-06-13 21:43:54 +0700

    refactor tabs: profile

--- project2/real-world-app/src/routes/profile/article-tabs.jsx
+++ project2/real-world-app/src/routes/profile/article-tabs.jsx
@@ -13,48 +13,48 @@ export const ArticleTabs = ({ profile }) => {
     return cs(
         consumeContext("apis"),
         consumeContext("auth"),
+        ({}, next) => EmptyFC({ next }),
+        ["location", ({}, next) => next(useLocation())],
+        [
+            "tabs",
+            ({ apis }, next) => {
+                const username = profile.username;
+                return next([
+                    {
+                        key: "my-articles",
+                        label: "My Articles",
+                        path: `/profile/${username}`,
+                        render: () =>
+                            ArticlePreviewList({
+                                getData: ({ page }) => apis.article.getMyFeed({ username, page }),
+                            }),
+                    },
+                    {
+                        key: "favorited-articles",
+                        label: "Favorited Articles",
+                        path: `/profile/${username}/favorite`,
+                        render: () =>
+                            ArticlePreviewList({
+                                getData: ({ page }) =>
+                                    apis.article.getFavoritedArticles({ username, page }),
+                            }),
+                    },
+                ]);
+            },
+        ],
 
-        // prettier-ignore
-        // ["username", ({ auth }, next) => auth.user.username === profile?.username
-        //     ? next(auth.user.username)
-        //     : next(profile?.username),
-        // ],
-
-        ({ apis }) => {
-            const username = profile.username;
-            const tabs = [
-                {
-                    key: "my-articles",
-                    label: "My Articles",
-                    path: `/profile/${username}`,
-                    render: () =>
-                        ArticlePreviewList({
-                            getData: ({ page }) => apis.article.getMyFeed({ username, page }),
-                        }),
-                },
-                {
-                    key: "favorited-articles",
-                    label: "Favorited Articles",
-                    path: `/profile/${username}/favorite`,
-                    render: () =>
-                        ArticlePreviewList({
-                            getData: ({ page }) =>
-                                apis.article.getFavoritedArticles({ username, page }),
-                        }),
-                },
-            ];
-            return Tabs({ tabs, onChangeTab: () => {} });
+        ({ tabs, location }) => {
+            return Tabs({ tabs, isActive: (tab) => tab.path === location.pathname });
         }
     );
 };
 
-const Tabs = ({ tabs }) => {
+const Tabs = ({ tabs, isActive }) => {
     // prettier-ignore
     return cs(
-        ({}, next) => EmptyFC({ next }),
-        ["location", ({}, next) => next(useLocation())],
-        ({ location }) => {
-            const activeTab = tabs.find((tab) => tab.path === location.pathname);
+
+        ({ }) => {
+            const activeTab = tabs.find((tab) => isActive(tab));
             return (
                 <>
                     <div className="feed-toggle">
@@ -64,9 +64,8 @@ const Tabs = ({ tabs }) => {
                                     <li key={i} className="nav-item">
                                         <Link to={tab.path} style={{ textDecoration: "none" }}>
                                             <div
-                                                style={{ cursor: "pointer" }}
                                                 className={cx1("nav-link", {
-                                                    active: tab.path === location.pathname,
+                                                    active: isActive(tab),
                                                 })}
                                             >
                                                 {tab.label}
